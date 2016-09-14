8d3ef45c0f9efadf1d4127d7e4b5f7316982c9b9
abdullahfde <abdullahfadel@std.sehir.edu.tr>
2016-09-15 02:15:41 +0300

    Update settings.py

--- recommender/settings.py
+++ recommender/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles', 'recommender', 'social.apps.django_app.default'
 )
 
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '254558244869-7oemcgmsh73m4vcgnsrlv98khvua05bv.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '739929149157-ao2o4n34uoghnm973qo30uhkub1h6n3r.apps.googleusercontent.com'
 
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'pnUgpMT7pJkq7SkJZ9lwCLLx'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'L-fHi8Ad4dEyi6NjTbJKoovu'
 AUTHENTICATION_BACKENDS = (
     'social.backends.google.GoogleOAuth2',
     'django.contrib.auth.backends.ModelBackend')
